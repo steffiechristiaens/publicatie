b737f2e109b26083a7898b87fa09c4f72a0dc094
steffiechristiaens <steffie-christiaens@hotmail.com>
2016-11-25 09:37:38 +0100

    25/11
    25/11

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -488,7 +488,7 @@
         var e = document.createElement("section");
         e.id = "top-1";
         e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.8.6';
+        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v3.9.2';
         document.body.insertBefore(e, document.body.childNodes[0]);
     }
 }();
